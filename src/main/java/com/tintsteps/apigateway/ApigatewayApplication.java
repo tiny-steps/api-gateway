434a70838a721d37e1b63a2fbaed3712ecd40c1d
m334701 <m334701@one.merckgroup.com>
2025-07-30 15:33:43 +0530

    updated resources

--- src/main/java/com/tintsteps/apigateway/ApigatewayApplication.java
+++ src/main/java/com/tintsteps/apigateway/ApigatewayApplication.java
@@ -1,4 +1,4 @@
-package com.tintsteps.api_gateway;
+package com.tintsteps.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tintsteps/apigateway/ApigatewayApplicationTests.java
+++ src/test/java/com/tintsteps/apigateway/ApigatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tintsteps.api_gateway;
+package com.tintsteps.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
