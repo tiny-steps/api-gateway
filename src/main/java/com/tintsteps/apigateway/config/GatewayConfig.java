0a7c50556eb640ce1d4371adcc0f75a60cca8f55
subh-m23 <subh.m@sascoders.com>
2025-08-02 18:32:19 +0530

    finished service layer of doctor service

--- src/main/java/com/tintsteps/apigateway/config/GatewayConfig.java
+++ src/main/java/com/tintsteps/apigateway/config/GatewayConfig.java
@@ -11,12 +11,16 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route("auth-service-oauth", r -> r.path("/oauth2/**", "/.well-known/**")
-                .uri("lb://auth-service"))
-            .route("auth-service-api", r -> r.path("/api/auth/**")
-                .uri("lb://auth-service"))
-            .route("ts-user-service", r -> r.path("/api/v1/users/**")
-                .uri("lb://ts-user-service"))
+                .route("auth-service-oauth", r -> r.path("/oauth2/**", "/.well-known/**")
+                        .uri("lb://auth-service"))
+                .route("auth-service-api", r -> r.path("/api/auth/**")
+                        .uri("lb://auth-service"))
+                .route("ts-user-service", r -> r.path("/api/v1/users/**")
+                        .uri("lb://ts-user-service"))
+                .route("ts-address-service", r -> r.path("/api/v1/addresses/**")
+                        .uri("lb://ts-address-service"))
+                .route("ts-doctor-service", r -> r.path("/api/v1/doctors/**")
+                        .uri("lb://ts-doctor-service"))
             .build();
     }
 }
