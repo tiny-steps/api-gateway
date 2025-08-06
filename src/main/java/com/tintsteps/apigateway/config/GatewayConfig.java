ab3a185ef47baddff3e3405632b327ca33499e59
subh-m23 <subh.m@sascoders.com>
2025-08-06 20:12:52 +0530

    updated deployment image

--- src/main/java/com/tintsteps/apigateway/config/GatewayConfig.java
+++ src/main/java/com/tintsteps/apigateway/config/GatewayConfig.java
@@ -11,9 +11,9 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("auth-service-oauth", r -> r.path("/oauth2/**", "/.well-known/**")
+                .route("ts-auth-service", r -> r.path("/oauth2/**", "/.well-known/**")
                         .uri("lb://ts-auth-service"))
-                .route("auth-service-api", r -> r.path("/api/auth/**")
+                .route("ts-auth-service", r -> r.path("/api/auth/**")
                         .uri("lb://ts-auth-service"))
                 .route("ts-user-service", r -> r.path("/api/v1/users/**")
                         .uri("lb://ts-user-service"))
