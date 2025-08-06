e0be2bb882f663da54fe1cf24a4094206da95b6e
subh-m23 <subh.m@sascoders.com>
2025-08-06 19:14:42 +0530

    updated k8s yml files and ci.yml files and gateway config

--- src/main/java/com/tintsteps/apigateway/config/GatewayConfig.java
+++ src/main/java/com/tintsteps/apigateway/config/GatewayConfig.java
@@ -12,9 +12,9 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service-oauth", r -> r.path("/oauth2/**", "/.well-known/**")
-                        .uri("lb://auth-service"))
+                        .uri("lb://ts-auth-service"))
                 .route("auth-service-api", r -> r.path("/api/auth/**")
-                        .uri("lb://auth-service"))
+                        .uri("lb://ts-auth-service"))
                 .route("ts-user-service", r -> r.path("/api/v1/users/**")
                         .uri("lb://ts-user-service"))
                 .route("ts-address-service", r -> r.path("/api/v1/addresses/**")
