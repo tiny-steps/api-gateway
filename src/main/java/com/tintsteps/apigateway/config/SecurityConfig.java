841068925586b2a6f719dd29759a91e3c445e194
subh-m23 <subh.m@sascoders.com>
2025-08-06 22:38:18 +0530

    modifed security configurations

--- src/main/java/com/tintsteps/apigateway/config/SecurityConfig.java
+++ src/main/java/com/tintsteps/apigateway/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.tintsteps.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
@@ -21,7 +26,7 @@ public class SecurityConfig {
                         .pathMatchers("/oauth2/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(spec -> spec.jwt(jwt -> jwt.jwtDecoder(jwtDecoder())));
 
         // Enforce statelessness by disabling the session-based security context repository.
         // This ensures that the security context is not saved between requests,
@@ -29,5 +34,10 @@ public class SecurityConfig {
         http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
 
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withIssuerLocation(this.issuerUri).build();
     }
 }
