92bd9f0f296f99ee5045e2bd9cc0dfe8e8a9c51f
subh-m23 <subh.m@sascoders.com>
2025-08-17 20:24:51 +0530

    added logging to filter

--- src/main/java/com/tintsteps/apigateway/config/JwtCookieToHeaderFilter.java
+++ src/main/java/com/tintsteps/apigateway/config/JwtCookieToHeaderFilter.java
@@ -10,14 +10,21 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 public class JwtCookieToHeaderFilter implements WebFilter {
     private static final String COOKIE_NAME = "jwt";
     private static final String AUTH_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
+    private static final Logger logger = LoggerFactory.getLogger(JwtCookieToHeaderFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        logger.info("JwtCookieToHeaderFilter invoked");
+        logger.info("Incoming cookies: {}", exchange.getRequest().getCookies());
+        logger.info("Incoming headers: {}", exchange.getRequest().getHeaders());
         List<String> cookies = exchange.getRequest().getCookies().containsKey(COOKIE_NAME)
                 ? exchange.getRequest().getCookies().get(COOKIE_NAME).stream().map(c -> c.getValue()).toList()
                 : List.of();
@@ -26,9 +33,10 @@ public class JwtCookieToHeaderFilter implements WebFilter {
             ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
                     .header(AUTH_HEADER, BEARER_PREFIX + jwt)
                     .build();
+            logger.info("Added Authorization header: {}{}", BEARER_PREFIX, jwt);
+            logger.info("Mutated headers: {}", mutatedRequest.getHeaders());
             return chain.filter(exchange.mutate().request(mutatedRequest).build());
         }
         return chain.filter(exchange);
     }
 }
-
